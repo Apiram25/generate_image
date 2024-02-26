ce13d40cadf397a916e9bb21c88cd94f539e2b84
amirabdrahimov <amirabdrahimov@gmail.com>
2024-02-26 15:41:25 +0100

    add generation of sequence of images

--- gen_imag.py
+++ gen_imag.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2 as cv
 
 # Create a black image
-img = np.zeros((512, 512), np.uint8)
+width = 512
+height = 700
+img = np.zeros((height, width), np.uint8)
+np.random.seed(42)
 
 # Parameters
 vanishing_point = (255, -255)
@@ -12,44 +15,60 @@ nb_of_rows = 11
 max_number_plants_per_row = 10
 
 # list containing row positions
-row_positions = []
+rows = []
 
+initial_plant_positions = []
 # Draw lines crossing in the vanishing point
 for i in range(nb_of_rows):
-    cv.line(img, (i * inter_row, 512), vanishing_point, 255, 1)
-
+    cv.line(img, (i * inter_row, height), vanishing_point, 255, 1)
     coordinates = np.where(img != 0)
     row_coordinates = np.array(list(zip(coordinates[1], coordinates[0])))
-    row_positions.append(row_coordinates)
-    img = np.zeros((512, 512), np.uint8)  # removing the lines
+    rows.append(row_coordinates)
+    img = np.zeros((height, width), np.uint8)
+    
+    # Generate initial positions of the plants
+    nb_plants = np.random.randint(1, max_number_plants_per_row)
+    random_selection = np.random.choice(height, nb_plants, replace=False)
+    initial_plant_positions.append(random_selection)
+    #print(initial_plant_positions)
+#initial_plant_positions = np.array(initial_plant_positions)
 
 # Adding lines (if needed)
-img = np.zeros((512, 512), np.uint8)
+img = np.zeros((height, width), np.uint8)
 for i in range(nb_of_rows):
-    cv.line(img, (i * inter_row, 512), vanishing_point, 255, 1)
+    cv.line(img, (i * inter_row, height), vanishing_point, 255, 1)   
+    
+# cv.imshow("Crop rows", img)
+# k = cv.waitKey(0)
 
-# list containing nb plants per row
+# Idea: move plants down across rows to create an image sequence
 nb_plants_per_row = []
+# find highest plant across all rows
+highest_plant = np.min(sum([list(sublist) for sublist in initial_plant_positions], []))
+for move_distance in range(0, height-1, 10):
+    # Continue generating new images until the highest put plant is not below the bottom third of the image
+    if highest_plant + move_distance > height/3 * 2:
+        break
+    # Draw lines on new image
+    img = np.zeros((height, width), np.uint8)
+    for i in range(nb_of_rows):
+        cv.line(img, (i * inter_row, height), vanishing_point, 255, 1)   
+    # Draw plants    
+    current_plant_positions = []
+    visible_plant_positions = []
+    for row_idx in range(len(rows)):
+        current_plant_positions = np.asarray(initial_plant_positions[row_idx] + move_distance)
+        visible_plant_positions = current_plant_positions[current_plant_positions < height]
+        
+        plant_positions = rows[row_idx][visible_plant_positions]
+        nb_plants = len(plant_positions)
+        for center in plant_positions:
+            if (center[0] < 0) or (center[1] < 0) or (center[0] > height) or (center[1] > height):
+                nb_plants = nb_plants - 1
+            else:
+                cv.circle(img, center, 7, 255, -1)  # drawing of the plants
+        nb_plants_per_row.append(nb_plants)  # adding the nb of plants to the global list
+    cv.imshow("Crop rows", img)
+    k = cv.waitKey(0)
+#print(nb_plants_per_row)
 
-# image sequence : we need to know how many plants are on the image at the moment
-# move up plant to down plant to create an image sequence
-
-for row in row_positions:
-    nb_plants = np.random.randint(1, max_number_plants_per_row)  # nb of plants in the row
-    # randmoly choosed coordinates
-    # from the list of coordinates of the row
-    random_selection = np.random.choice(len(row), nb_plants, replace=False)
-    plant_positions = row[random_selection]  # coordinates of plants in the row
-    # img = np.zeros((512, 512), np.uint8)
-    for center in plant_positions:
-        if (center[0] < 0) or (center[1] < 0) or (center[0] > 512) or (center[1] > 512):
-            nb_plants = nb_plants - 1
-        else:
-            cv.circle(img, center, 7, 255, -1)  # drawing of the plants
-    nb_plants_per_row.append(nb_plants)  # adding the nb of plants to the global list
-
-print(nb_plants_per_row)
-
-# Showing the images
-cv.imshow("Crop rows", img)
-k = cv.waitKey(0)
---
+++ gen_image.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2 as cv
+
+np.random.seed(43)
+
+# Parameters
+height = 700
+width = 512
+vp_height = -500
+vanishing_point = (255, vp_height)
+inter_row_distance = 80
+nb_of_rows = 7
+max_number_plants_per_row = 20
+
+img = np.zeros((height, width), np.uint8)
+
+# Get position of lines crossing in the vanishing point
+rows = []
+initial_plant_positions = []
+for i in range(nb_of_rows):
+    cv.line(img, (i * inter_row_distance, height), vanishing_point, 255, 1)
+    coordinates = np.where(img != 0)
+    row_coordinates = np.array(list(zip(coordinates[1], coordinates[0])))
+    rows.append(row_coordinates)
+    img = np.zeros((height, width), np.uint8)
+    
+    # Generate initial positions of the plants
+    nb_plants = np.random.randint(1, max_number_plants_per_row)
+    random_selection = np.random.choice(np.arange(vp_height, height, 50), nb_plants, replace=False)
+    initial_plant_positions.append(random_selection)
+
+# Idea: move plants down across rows to create an image sequence
+# Find highest plant across all rows
+highest_plant = np.min(sum([list(sublist) for sublist in initial_plant_positions], []))
+for move_distance in range(0, -1 * vp_height + height-1, 10):
+    # Continue generating new images until the highest put plant is not below the bottom third of the image
+    if (highest_plant + move_distance) > height/3 * 2:
+        break
+    # Draw lines of rows
+    img = np.zeros((height, width), np.uint8)
+    for i in range(nb_of_rows):
+        cv.line(img, (i * inter_row_distance, height), vanishing_point, 255, 1)   
+    # Draw plants    
+    nb_plants_per_row = []
+    current_plant_positions = []
+    visible_plant_positions = []
+    for row_idx in range(len(rows)):
+        current_plant_positions = np.asarray(initial_plant_positions[row_idx] + move_distance)
+        visible_plant_positions = current_plant_positions[current_plant_positions < height]     
+        plant_positions = rows[row_idx][visible_plant_positions]
+        nb_plants = len(plant_positions)
+        for center in plant_positions:
+            if (center[0] < 0) or (center[1] < 0) or (center[0] > width) or (center[1] > height):
+                nb_plants = nb_plants - 1
+            else:
+                cv.circle(img, center, 12, 255, -1)  # drawing of the plants
+        nb_plants_per_row.append(nb_plants) 
+    #print(nb_plants_per_row)
+    cv.imshow("Crop rows", img)
+    k = cv.waitKey(0)
+
