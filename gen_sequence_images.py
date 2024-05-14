cf9f3826fa395e64e32acbb6f9437f7e1d901b1a
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-05-14 15:16:48 +0200

    added Word class

--- gen_sequence_images.py
+++ gen_sequence_images.py
@@ -1,32 +1,62 @@
 import numpy as np
 import cv2 as cv
 
-# Visualizer draws plants and particles at the same time
-class Visualizer():
-    def __init__(self, plants, particles):
-        self.plants = plants
-        self.particles = particles
 
-    def draw_plants(self, move_distance):
-        pass
+# Visualizer draws plants and particles at the same time
+class Visualizer:
+    def __init__(self, world):
+        self.world = world
+        self.img = np.zeros((world.height, world.width), np.uint8)
+
+    def draw_plants(self, plants):
+        for row_idx in range(plants.nb_rows):
+            plant_positions, plant_types = plants.getPlantsToDraw(row_idx)
+            # Draw
+            for i, center in enumerate(plant_positions):
+                if (center[0] < 0) or (center[1] < 0) or (center[0] > self.world.width) or (center[1] > self.world.height):
+                    # nb_visible_plants = nb_visible_plants - 1
+                    pass
+                else:
+                    perspective_coef = center[1] / self.world.height
+                    if plant_types[i] == 0:
+                        cv.circle(self.img, center, int(20 * perspective_coef), 255, -1)
+                    elif plant_types[i] == 1:
+                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_CROSS,
+                                      markerSize=int(50 * perspective_coef), thickness=5)
+                    elif plant_types[i] == 2:
+                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_TILTED_CROSS,
+                                      markerSize=int(50 * perspective_coef), thickness=5)
+                    else:
+                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_STAR,
+                                      markerSize=int(50 * perspective_coef), thickness=5)
 
     def draw_particles(self):
         pass
 
-    def draw(self, move_distance):
-        self.draw_plants(move_distance)
+    def draw(self, plants):
+        # Empty image
+        self.img = np.zeros((world.height, world.width), np.uint8)
+
+        # Draw plants and particles
+        self.draw_plants(plants)
         self.draw_particles()
 
-        cv.imshow("Crop rows", plants.img)
-        k = cv.waitKey(0)
+        cv.imshow("Crop rows", self.img)
+        cv.waitKey(0)
+
+
+class World:
+    def __init__(self, width, height, speed):
+        self.width = width
+        self.height = height
+        self.speed = speed
+
 
 # Weeds needs to be added
-class Plants():
-    def __init__(self, height, width, vp_height, vp_width, ir, ip, o, s, c, nb_rows, nb_plant_types):
-        # Image parameters
-        self.height = height
-        self.width = width
-        self.img = np.zeros((height, width), np.uint8)
+class Plants:
+    def __init__(self, world, vp_height, vp_width, ir, ip, o, s, c, nb_rows, nb_plant_types):
+        # World contains the width and height of the image
+        self.world = world
 
         # Parameters to generate image and to be tracked
         self.inter_row_distance = ir
@@ -39,101 +69,126 @@ class Plants():
         self.vp_height = vp_height
         self.vp_width = vp_width + self.offset
         self.vanishing_point = (vp_width, vp_height)
-        self.vp_distance = np.absolute(height - vp_height)
 
         # Plants generation parameters
-        self.nb_of_plants_rows = nb_rows
-        self.max_number_plants_per_row = np.ceil(self.vp_distance / self.inter_plant_distance)  # modified
+        vp_distance = np.absolute(world.height - vp_height)
+
+        self.nb_rows = nb_rows
         self.nb_plant_types = nb_plant_types
+        self.max_number_plants_per_row = np.ceil(vp_distance / self.inter_plant_distance)
 
-        # Particular plants' height
-        self.highest_plant = -1
-        self.tracked_plant = -1
-
-        #
-        self.initial_plant_positions = []
+        # List containing positions of all the plants
+        self.plant_positions = []
         self.plants_rows = []
         self.plant_types = []
 
     def generate_plants(self):
-        for i in range(self.nb_of_plants_rows):
+        for i in range(self.nb_rows):
+            # Empty image
+            img = np.zeros((self.world.height, self.world.width), np.uint8)
+
             # Get position of lines crossing in the vanishing point
-            cv.line(self.img, (i * self.inter_row_distance + self.offset, self.height), self.vanishing_point, 255, 1)
-            coordinates = np.where(self.img != 0)
+            cv.line(img, (i * self.inter_row_distance + self.offset, self.world.height), self.vanishing_point, 255, 1)
+            coordinates = np.where(img != 0)
             row_coordinates = np.array(list(zip(coordinates[1], coordinates[0])))
             self.plants_rows.append(row_coordinates)
-            self.img = np.zeros((self.height, self.width), np.uint8)
 
-            # Generate initial positions of the plants
-            nb_plants = np.random.randint(1, self.max_number_plants_per_row)
-            print("max number of plants per row : {}".format(self.max_number_plants_per_row))
-            print("nb_plants : {}".format(nb_plants))
-            # Modified for the particle filter
-            random_selection = np.random.choice(np.arange(self.vp_height, self.height, self.inter_plant_distance), nb_plants, replace=False)
-            self.initial_plant_positions.append(random_selection)
+            # Generate initial positions of the plants for a row
+            # number of plants in the row : between 60% and 100% of the maximum number of plants per row
+            nb_plants = np.random.randint(np.floor(0.60 * self.max_number_plants_per_row), self.max_number_plants_per_row)
+            # random positions for each plant
+            random_selection = np.random.choice(np.arange(self.vp_height, self.world.height, self.inter_plant_distance),
+                                                nb_plants, replace=False)
+            # add of a little noise
+            for i in range(len(random_selection)):
+                selection = random_selection[i]
+                # Gaussian centered around the original coordinates
+                random_selection[i] = np.random.normal(selection, 6,1)
+
+            self.plant_positions.append(random_selection)
+
+            # Mapping a type for each plant
             plant_markers = np.random.choice(self.nb_plant_types, nb_plants)
             self.plant_types.append(plant_markers)
 
         # Find highest plant across all rows
-        self.highest_plant = np.min(sum([list(sublist) for sublist in self.initial_plant_positions], []))
+        # self.highest_plant = np.min(sum([list(sublist) for sublist in self.initial_plant_positions], []))
 
     def move(self, move_distance):
         # Idea: move plants down across rows to create an image sequence
 
         # Stop generating images when the last plant is below the bottom third of the image
-        if (self.highest_plant + move_distance) > self.height/3 * 2:
-            return
+        # if (self.highest_plant + move_distance) > self.height/3 * 2:
+        # return
 
-        self.img = np.zeros((self.height, self.width), np.uint8)
+        self.img = np.zeros((self.world.height, self.world.width), np.uint8)
 
-        # Draw plants
-        nb_plants_per_row = []
-        current_plants_positions = []
-        visible_plants_positions = []
-        for row_idx in range(self.nb_of_plants_rows):
-            current_plants_positions = np.asarray(self.initial_plant_positions[row_idx] + move_distance)
-            last_point_of_row = self.plants_rows[row_idx][-1][1]
-            current_row_plants_types = self.plant_types[row_idx][(current_plants_positions >= 0) & (current_plants_positions <= last_point_of_row)]
-            visible_plants_positions = current_plants_positions[(current_plants_positions >= 0) & (current_plants_positions <= last_point_of_row)]
-            # print("Visible plant positions : {}".format(visible_plants_positions))
-            plant_positions = self.plants_rows[row_idx][visible_plants_positions]
-            nb_visible_plants = len(plant_positions)
-
-            # The tracked plant's height
-            if (row_idx == np.floor(self.nb_of_plants_rows/2)):
-                try:
-                    self.tracked_plant = np.max(visible_plants_positions)
-                except:
-                    self.tracked_plant = -1
-                    print("The last plant to track has gone")
-
-
-            for i, center in enumerate(plant_positions):
-                if (center[0] < 0) or (center[1] < 0) or (center[0] > self.width) or (center[1] > self.height):
-                    nb_visible_plants = nb_visible_plants - 1
-                else:
-                    perspective_coef = center[1]/self.height
-                    if current_row_plants_types[i] == 0:
-                        cv.circle(self.img, center, int(20 * perspective_coef), 255, -1)
-                    elif current_row_plants_types[i] == 1:
-                        cv.drawMarker(self.img,center,255,markerType = cv.MARKER_CROSS,markerSize = int(50 * perspective_coef),thickness = 5)
-                    elif current_row_plants_types[i] == 2:
-                        cv.drawMarker(self.img,center,255,markerType = cv.MARKER_TILTED_CROSS,markerSize = int(50 * perspective_coef),thickness = 5)
-                    else:
-                        cv.drawMarker(self.img,center,255,markerType = cv.MARKER_STAR,markerSize = int(50 * perspective_coef),thickness = 5)
-            nb_plants_per_row.append(nb_visible_plants)
-            #print(nb_plants_per_row)
+        # Move every plants using the input move_distance value
+        for row_idx in range(self.nb_rows):
+            for i in range(len(self.plant_positions[row_idx])):
+                self.plant_positions[row_idx][i] += move_distance
 
     def measure(self):
-        return self.tracked_plant
+        """
+        Returns the height of the tracked plant.
+        The tracked plant being the plant of the middle row that is the closest to the bottom of the image
+        """
+
+        tracked_plant_height = -1
+
+        for row_idx in range(self.nb_rows):
+            # If we are in the middle row
+            if row_idx == np.floor(self.nb_rows / 2):
+                current_plants_positions = np.asarray(self.plant_positions[row_idx])
+                last_point_of_row = self.plants_rows[row_idx][-1][1]
+                visible_plants_positions = current_plants_positions[
+                    (current_plants_positions >= 0) & (current_plants_positions <= last_point_of_row)]
+
+                try:
+                    tracked_plant_height = np.max(visible_plants_positions)
+                except:
+                    print("The last plant to track has gone")
+
+        return tracked_plant_height
+
+    def getPlantsToDraw(self, row_idx):
+        """
+        Returns a list containing the positions and a list containing the type of the plants to draw given a row index
+
+        :param row_idx:
+        :returns (list containing the positions, list containing the types):
+        """
+
+        if row_idx < 0 or row_idx >= self.nb_rows:
+            print("Error the row index is incorrect")
+            return -1
+
+        current_plants_positions = np.asarray(self.plant_positions[row_idx])
+        last_point_of_row = self.plants_rows[row_idx][-1][1]
+        visible_plants_positions = current_plants_positions[
+            (current_plants_positions >= 0) & (current_plants_positions <= last_point_of_row)]
+
+        plant_positions = self.plants_rows[row_idx][visible_plants_positions]
+        plant_types = self.plant_types[row_idx][
+            (current_plants_positions >= 0) & (current_plants_positions <= last_point_of_row)]
+
+        return plant_positions, plant_types
 
 
-# main
-plants = Plants(700, 500, -500, 250, 80, 110, o=0, s=0, c=0, nb_rows=7, nb_plant_types=4)
+# Main
+# Initialize world
+world = World(500, 700, 10)
+
+# Initialize visualizer
+visualizer = Visualizer(world)
+
+# Initialize plants
+plants = Plants(world, -500, 250, 80, 110, o=0, s=0, c=0, nb_rows=7, nb_plant_types=4)
 plants.generate_plants()
 
-for move_distance in range(0, -1 * plants.vp_height + plants.height-1, 10):
-    plants.move(move_distance)
+# Moving the plants
+for i in range(0, -1 * plants.vp_height + world.height - 1, 10):
+    plants.move(world.speed)
     print("Tracked plant's height : {}".format(plants.measure()))
-    cv.imshow("Crop rows", plants.img)
-    k = cv.waitKey(0)
+
+    visualizer.draw(plants)
