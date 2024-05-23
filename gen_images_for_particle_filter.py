5e2f3a1127fc9b829654bd735c68defe5ae5fd17
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-05-23 10:04:46 +0200

    update

--- gen_images_for_particle_filter.py
+++ gen_images_for_particle_filter.py
@@ -2,7 +2,14 @@ import numpy as np
 import cv2 as cv
 
 
-# Visualizer draws plants and particles at the same time
+class World:
+    def __init__(self, width, height, speed):
+        self.width = width
+        self.height = height
+        self.speed = speed
+
+
+# Visualizer draws plants and particles
 class Visualizer:
     def __init__(self, world):
         self.world = world
@@ -13,48 +20,137 @@ class Visualizer:
             plant_positions, plant_types = plants.getPlantsToDraw(row_idx)
             # Draw
             for i, center in enumerate(plant_positions):
-                if (center[0] < 0) or (center[1] < 0) or (center[0] > self.world.width) or (center[1] > self.world.height):
+                if (center[0] < 0) or (center[1] < 0) or (center[0] > self.world.width) or (
+                        center[1] > self.world.height):
                     # nb_visible_plants = nb_visible_plants - 1
                     pass
                 else:
                     perspective_coef = center[1] / self.world.height
                     if plant_types[i] == 0:
-                        cv.circle(self.img, center, int(20 * perspective_coef), 255, -1)
+                        cv.circle(self.img, center, int(20 * perspective_coef), (255, 255, 255), -1)
                     elif plant_types[i] == 1:
-                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_CROSS,
+                        cv.drawMarker(self.img, center, (255, 255, 255), markerType=cv.MARKER_CROSS,
                                       markerSize=int(50 * perspective_coef), thickness=5)
                     elif plant_types[i] == 2:
-                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_TILTED_CROSS,
+                        cv.drawMarker(self.img, center, (255, 255, 255), markerType=cv.MARKER_TILTED_CROSS,
                                       markerSize=int(50 * perspective_coef), thickness=5)
                     else:
-                        cv.drawMarker(self.img, center, 255, markerType=cv.MARKER_STAR,
+                        cv.drawMarker(self.img, center, (255, 255, 255), markerType=cv.MARKER_STAR,
                                       markerSize=int(50 * perspective_coef), thickness=5)
 
-    def draw_particles(self):
-        pass
+    def draw_particles(self, particles, n):
+        for i in range(n):
+            # Coordinates of the particle
+            center = np.asarray([int(self.world.width / 2), int(particles[i][1][0])])
 
-    def draw(self, plants):
+            perspective_coef = center[1] / self.world.height
+
+            # Color of the particle in fonction of the its weight
+            if particles[i][0] > 0.70:
+                color = (0, 0, 255)
+                thickness = 5
+            elif particles[i][0] > 0.30:
+                color = (255, 0, 0)
+                thickness = 4
+            else:
+                color = (0, 255, 0)
+                thickness = 2
+
+            cv.drawMarker(self.img, center, color, markerType=cv.MARKER_DIAMOND,
+                          markerSize=int((7 * thickness) * perspective_coef), thickness=thickness)
+
+    def draw_complete_particle(self, offset, position, ir, skew, convergence, ip):
+
+        # Drawing of the particular particle using offset and position
+        self.draw_particular_plant(offset, position)
+
+        # Drawing the horizontally neighboring plants to the particular plant
+        self.draw_horizontal_neighbors(offset, position, ir)
+
+    def draw_particular_plant(self, offset, position):
+        if not (self.is_valid_coordinates(offset, position)):
+            print("Error: offset and/or position has an invalid value")
+            return -1
+        center = np.asarray([offset, position])
+
+        perspective_coef = center[1] / self.world.height
+        color = (0, 255, 0)
+
+        cv.drawMarker(self.img, center, color, markerType=cv.MARKER_DIAMOND,
+                      markerSize=int(40 * perspective_coef), thickness=4)
+
+    def draw_horizontal_neighbors(self, offset, position, ir):
+        """
+        Draws the horizontal neighbours of the particular plant, in other words plants that are located to the left,
+        right and at the same height as the particular plant.
+        """
+
+        # Drawing parameters
+        perspective_coef = position / self.world.height
+        color = (255, 0, 0)
+
+        # Drawing left neighbors
+        leftOffset = offset - ir
+
+        while self.is_valid_coordinates(leftOffset, position):
+            center = np.asarray([leftOffset, position])
+
+            cv.drawMarker(self.img, center, color, markerType=cv.MARKER_DIAMOND,
+                          markerSize=int(40 * perspective_coef), thickness=4)
+
+            leftOffset -= ir
+
+        # Drawing right neighbors
+        rightOffset = offset + ir
+
+        while self.is_valid_coordinates(rightOffset, position):
+            center = np.asarray([rightOffset, position])
+
+            cv.drawMarker(self.img, center, color, markerType=cv.MARKER_DIAMOND,
+                          markerSize=int(40 * perspective_coef), thickness=4)
+
+            rightOffset += ir
+
+    def draw_top_particular_plant(self, offset, position, skew):
+        return True
+
+    def is_valid_coordinates(self, x, y):
+        """
+        Returns a boolean indicating whether (x, y) is inside the image or not (valid position or not).
+        """
+        if x < 0 or x > self.world.width:
+            return False
+
+        if y < 0 or y > self.world.height:
+            return False
+
+        return True
+
+    def draw(self, plants, particles, n_particles):
         # Empty image
-        self.img = np.zeros((self.world.height, self.world.width), np.uint8)
+        self.img = np.zeros((self.world.height, self.world.width, 3), np.uint8)
 
         # Draw plants and particles
         self.draw_plants(plants)
-        self.draw_particles()
+        self.draw_particles(particles, n_particles)
 
-        cv.imshow("Crop rows", self.img)
-        cv.waitKey(0)
+        # Testing of the function draw_complete_particle
+        #self.img = np.zeros((self.world.height, self.world.width, 3), np.uint8)
 
+        offset = 250
+        position = self.world.height - 40
+        ir = 70
 
-class World:
-    def __init__(self, width, height, speed):
-        self.width = width
-        self.height = height
-        self.speed = speed
+        #self.draw_complete_particle(offset, position, ir, -1, -1, -1)
+
+    def clear(self):
+        self.img = np.zeros((self.world.height, self.world.width, 3), np.uint8)
 
 
 # Weeds needs to be added
 class Plants:
     def __init__(self, world, vp_height, vp_width, ir, ip, o, s, c, nb_rows, nb_plant_types):
+        # Initialize plants positions
         # World contains the width and height of the image
         self.world = world
 
@@ -62,8 +158,8 @@ class Plants:
         self.inter_row_distance = ir
         self.inter_plant_distance = ip
         self.offset = o  # between -(width/2) and +(width/2)
-        #self.skew = s  # TODO
-        #self.convergence = c  # TODO
+        self.skew = s  # TODO
+        self.convergence = c  # TODO
 
         # Field parameters
         self.vp_height = vp_height
@@ -82,6 +178,12 @@ class Plants:
         self.plants_rows = []
         self.plant_types = []
 
+        # Initialize standard deviation noise for plants motion
+        self.std_move_distance = 0
+
+        # Initialize standard deviation noise for plants position measurement
+        self.std_meas_position = 0
+
     def generate_plants(self):
         for i in range(self.nb_rows):
             # Empty image
@@ -95,7 +197,8 @@ class Plants:
 
             # Generate initial positions of the plants for a row
             # number of plants in the row : between 70% and 100% of the maximum number of plants per row
-            nb_plants = np.random.randint(np.floor(0.70 * self.max_number_plants_per_row), self.max_number_plants_per_row)
+            nb_plants = np.random.randint(np.floor(0.70 * self.max_number_plants_per_row),
+                                          self.max_number_plants_per_row)
             # random positions for each plant
             random_selection = np.random.choice(np.arange(self.vp_height, self.world.height, self.inter_plant_distance),
                                                 nb_plants, replace=False)
@@ -103,7 +206,7 @@ class Plants:
             for j in range(len(random_selection)):
                 selection = random_selection[j]
                 # Gaussian centered around the original coordinates
-                random_selection[j] = np.random.normal(selection, 11,1)
+                random_selection[j] = np.random.normal(selection, 11, 1)
 
             self.plant_positions.append(random_selection)
 
@@ -114,40 +217,21 @@ class Plants:
         # Find highest plant across all rows
         # self.highest_plant = np.min(sum([list(sublist) for sublist in self.initial_plant_positions], []))
 
-    def move(self, move_distance):
-        # Move every plants using the input move_distance value
+    def move(self, desired_move_distance):
+        # Compute relative motion (true motion is desired motion with some noise)
+        move_distance = np.random.normal(loc=desired_move_distance, scale=self.std_move_distance, size=1)[0]
+
+        # Move every plants
         for row_idx in range(self.nb_rows):
             for i in range(len(self.plant_positions[row_idx])):
                 self.plant_positions[row_idx][i] += move_distance
 
-    def measure(self):
-        """
-        Returns the height of the tracked plant.
-        The tracked plant being the plant of the middle row that is the closest to the bottom of the image
-        """
-
-        tracked_plant_height = -1
-
-        for row_idx in range(self.nb_rows):
-            # If we are in the middle row
-            if row_idx == np.floor(self.nb_rows / 2):
-                current_plants_positions = np.asarray(self.plant_positions[row_idx])
-                last_point_of_row = self.plants_rows[row_idx][-1][1]
-                visible_plants_positions = current_plants_positions[
-                    (current_plants_positions >= 0) & (current_plants_positions <= last_point_of_row)]
-
-                try:
-                    tracked_plant_height = np.max(visible_plants_positions)
-                except:
-                    print("The last plant to track has gone")
-
-        return tracked_plant_height
-
     def getPlantsToDraw(self, row_idx):
         """
-        Returns a list containing the positions and a list containing the type of the plants to draw given a row index
+        Returns a list containing the positions and a list containing the type of the plants to draw given a row index.
+        Used by the visualizer.
 
-        :param row_idx:
+        :param row_idx: Index of the row where we look for plants
         :returns (list containing the positions, list containing the types):
         """
 
@@ -175,12 +259,23 @@ world = World(500, 700, 10)
 visualizer = Visualizer(world)
 
 # Initialize plants
-plants = Plants(world, -500, 250, 80, 110, o=0, s=0, c=0, nb_rows=7, nb_plant_types=4)
+plants = Plants(world, -100, 250, 80, 110, o=0, s=0, c=0, nb_rows=7, nb_plant_types=4)
 plants.generate_plants()
 
-# Moving the plants
-for i in range(0, -1 * plants.vp_height + world.height - 1, 10):
-    plants.move(world.speed)
-    print("Tracked plant's height : {}".format(plants.measure()))
+# Number of steps
+steps = 70
 
-    visualizer.draw(plants)
+# Setpoint (desired) motion distance
+plants_setpoint_motion_move_distance = 11
+
+# Visualize
+for i in range(steps):
+    # Simulate plants motion (required motion will not excatly be achieved)
+    plants.move(plants_setpoint_motion_move_distance)
+
+    # Visualization
+    visualizer.clear()
+    visualizer.draw_plants(plants)
+    cv.imshow("Crop rows", visualizer.img)
+    cv.waitKey(0) # Show maximum normalized particle weight (converges to 1.0) and correctness (0 = correct)
+
